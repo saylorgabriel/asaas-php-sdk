ae838c5b86139ab98e99de49a3b57d19cbd7fe32
saylor.damasceno <saylor@consolidesuamarca.com.br>
2024-09-19 13:12:04 -0300

    Atualizando dominio da api

--- src/Api/AbstractApi.php
+++ src/Api/AbstractApi.php
@@ -16,14 +16,14 @@ abstract class AbstractApi
      *
      * @var string
      */
-    const ENDPOINT_PRODUCAO = 'https://www.asaas.com/api/v2';
+    const ENDPOINT_PRODUCAO = 'https://api.asaas.com/v3';
 
     /**
      * Endpoint Homologação
      *
      * @var string
      */
-    const ENDPOINT_HOMOLOGACAO = 'http://homolog.asaas.com/api/v2';
+    const ENDPOINT_HOMOLOGACAO = 'https://sandbox.asaas.com/api/v3';
 
     /**
      * Endpoint Sandbox
